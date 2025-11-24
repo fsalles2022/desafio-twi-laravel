1436531cd8932cf5663bd9fa97b848600acbba41
fsalles.wtime <fsalles.wtime@out.bionexo.com>
2025-11-23 23:19:06 -0300

    feat: add default role column to users table migration

--- desafio-laravel/database/migrations/2025_09_29_230227_create_users_table.php
+++ desafio-laravel/database/migrations/2025_09_29_230227_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('image')->nullable();
+            $table->string('role')->default('student');
             $table->timestamps();
         });
     }
